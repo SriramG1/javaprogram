e8782a3c99788ac95c3adb377bfaaec573b6be5c
SriramG1 <sriramhht@gmail.com>
2022-05-14 15:27:22 +0530

    Banking system using json object

---
+++ src/weekone/bankingsystem/usingjson/UserDetails.java
@@ -0,0 +1,41 @@
+package weekone.bankingsystem.usingjson;
+
+public class UserDetails {
+
+    private String name;
+    private String accountNumber;
+    private String accountType;
+    private String balance;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public String getBalance() {
+        return balance;
+    }
+
+    public void setBalance(String balance) {
+        this.balance = balance;
+    }
+}
